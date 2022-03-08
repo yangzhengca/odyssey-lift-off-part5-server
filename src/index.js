991d64b6cf209e28f7786db7478893bb1208eb04
yangzhengca <yangzhengca@gmail.com>
2022-03-07 23:11:00 -0600

    comment ApolloServerPluginSchemaReporting

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 const TrackAPI = require('./datasources/track-api');
-import { ApolloServerPluginSchemaReporting } from "apollo-server-core";
+// const { ApolloServerPluginSchemaReporting } = require("apollo-server-core");
 
 async function startApolloServer(typeDefs, resolvers) {
   const server = new ApolloServer({
@@ -13,11 +13,15 @@ async function startApolloServer(typeDefs, resolvers) {
         trackAPI: new TrackAPI(),
       };
     },
-    plugins: [
-      ApolloServerPluginSchemaReporting({
-        endpointUrl: "https://schema-reporting.api.staging.c0.gql.zone/api/graphql"
-      }),
-    ],
+    // apollo: {
+    //   // You can also just set $APOLLO_GRAPH_VARIANT.
+    //   graphVariant: 'current',
+    // },
+    // plugins: [
+    //   ApolloServerPluginSchemaReporting({
+    //     endpointUrl: "https://schema-reporting.api.staging.c0.gql.zone/api/graphql"
+    //   }),
+    // ],
   });
 // await server.listen({port: process.env.PORT || 4000});
   const { url, port } = await server.listen({port: process.env.PORT || 4000});
