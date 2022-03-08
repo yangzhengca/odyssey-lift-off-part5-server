fa7c51cf65dfdafc85cc933901481dd1fc7c6a3d
yangzhengca <yangzhengca@gmail.com>
2022-03-07 22:53:48 -0600

    added schema reporting plugin

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 const TrackAPI = require('./datasources/track-api');
+import { ApolloServerPluginSchemaReporting } from "apollo-server-core";
 
 async function startApolloServer(typeDefs, resolvers) {
   const server = new ApolloServer({
@@ -12,6 +13,11 @@ async function startApolloServer(typeDefs, resolvers) {
         trackAPI: new TrackAPI(),
       };
     },
+    plugins: [
+      ApolloServerPluginSchemaReporting({
+        endpointUrl: "https://schema-reporting.api.staging.c0.gql.zone/api/graphql"
+      }),
+    ],
   });
 // await server.listen({port: process.env.PORT || 4000});
   const { url, port } = await server.listen({port: process.env.PORT || 4000});
